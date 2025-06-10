5c85b7e21710b08fc01587e69449fc4f213eca54
Lilia Ould Hocine <liliaohl@gmail.com>
2025-06-10 17:00:33 -0400

    Ajout d'une solution alternative

--- solutions/fonctions/callbacks/exo5.js
+++ solutions/fonctions/callbacks/exo5.js
@@ -3,3 +3,13 @@ afficherMessage("1", 1000, () => {
     afficherMessage("3", 3000, () => {});
   });
 });
+
+// alternative
+
+const rien = () => {
+  console.log("C'est fini !");
+};
+const afficher3apres3sec = () => afficherMessage(3, 3000, rien);
+const afficher2apres2sec = () => afficherMessage(2, 2000, afficher3apres3sec);
+
+afficherMessage(1, 1000, afficher2apres2sec);
